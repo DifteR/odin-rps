e96bcbde6e809e0b8ff73890b9e4d80475948cc8
Tilen Marš <marstilen@gmail.com>
2022-09-15 17:01:56 +0200

    Create the winner algorithm

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+const playOptions = ["rock", "paper", "scissors"];
+
 function computerPlay() {
-  const playOptions = ["rock", "paper", "scissors"];
   let x = Math.floor(Math.random() * 3);
   return playOptions[x];
 }
 
-computerPlay();
+function getWinner(pick1, pick2) {
+  console.log(pick1, pick2);
+  if (
+    playOptions.indexOf(pick1) - playOptions.indexOf(pick2) == 1 ||
+    playOptions.indexOf(pick1) - playOptions.indexOf(pick2) == -2
+  ) {
+    console.log("player 1 wins");
+  } else if (
+    playOptions.indexOf(pick1) - playOptions.indexOf(pick2) != 0 ||
+    playOptions.indexOf(pick1) - playOptions.indexOf(pick2) == 2
+  ) {
+    console.log("player 2 wins");
+  } else {
+    console.log("It's a draw");
+  }
+}
+
+let player1Picks = computerPlay();
+let player2Picks = computerPlay();
+getWinner(player1Picks, player2Picks);
